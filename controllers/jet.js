b3aa8e8f79c9b0747478ba0aedbd073dd5f8c276
Gopikrishna29 <89415620+Gopikrishna29@users.noreply.github.com>
2021-11-12 21:34:17 -0600

    intial commit for lab12

--- controllers/jet.js
+++ controllers/jet.js
@@ -46,17 +46,17 @@ exports.jet_update_put = function(req, res) {
     res.send('NOT IMPLEMENTED: jet update PUT' + req.params.id); 
 }; 
 
-// Handle Costume create on POST. 
+// Handle jet create on POST. 
 exports.jet_create_post = async function(req, res) { 
     console.log(req.body) 
     let document = new jet(); 
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"jet_type":"goat", "jet_model":12, "cost":"large"} 
+    // {"jet_type":"goat", "jet_model":12, "jet_model":"large"} 
     document.jet_type = req.body.jet_type; 
     document.jet_model = req.body.jet_model; 
-    document.cost = req.body.cost; 
+    document.jet_model = req.body.jet_model; 
     try{ 
         let result = await document.save(); 
         res.send(result); 
@@ -66,3 +66,36 @@ exports.jet_create_post = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+
+// for a specific jet. 
+exports.jet_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await jet.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
+
+// Handle jet update form on PUT. 
+exports.jet_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await jet.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.jet_type)  
+               toUpdate.jet_type = req.body.jet_type; 
+        if(req.body.jet_model) toUpdate.jet_model = req.body.jet_model; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+};
